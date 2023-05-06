d5c390c40aa3e13834d13fab77eae57df7a0bb99
Mr_RB <50051776+rayen-brigui@users.noreply.github.com>
2023-05-06 12:08:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MegaTek</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+				<header id="header" class="alt">
+				<a href="index.php" class="logo"><strong> MegaTek </strong> <span>Store</span></a>
+					<nav>
+						<a href="#menu">Menu</a>
+					</nav>
+				</header>
+
+				<!-- Menu -->
+				<nav id="menu">
+					<ul class="links">
+		                <li class="active"> <a href="index.html">Home </a> </li>
+
+		                <li> <a href="products.html">Products</a> </li>
+
+		                <li> <a href="checkout.html">Checkout</a> </li>
+
+		                <li><a href="contact.html">Contact Us</a></li>
+
+		                <li class="dropdown">
+		                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About</a>
+		                    
+		                    <ul class="dropdown-menu">
+		                        <li> <a href="about-us.html">About Us</a> </li>
+				                <li><a href="team.html">Team</a></li>
+				                <li> <a href="blog.html">Blog</a> </li>
+
+				                <li><a href="testimonials.html">Testimonials</a></li>
+				                
+				                <li><a href="terms.html">Terms</a></li>
+		                    </ul>
+		                </li>
+            		</ul>
+				</nav>
+
+				<!-- Banner -->
+					<section id="banner" class="major">
+						<div class="inner">
+							<header class="major">
+								<h1>All what you need in one place</h1>
+							</header>
+							<div class="content">
+								<p>Upgrade Your Tech Game with Our Premium Computer Hardware Store - Shop Now!</p>
+								<ul class="actions">
+									<li><a href="contact.html" class="button next scrolly">Contact Us</a></li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Main -->
+				<div id="main">
+					
+					
+					<!-- About us -->
+					<section>
+						<div class="inner">
+							<header class="major">
+								<h2>About us</h2>
+							</header>
+							<p>At our store, we pride ourselves on providing exceptional customer service. We are passionate about technology, and we strive to share our knowledge and expertise with our customers, so you can make informed decisions about the products you purchase. Whether you're a gamer, a creative professional, or a business owner, we have the products you need to power your devices and stay ahead of the curve.
+
+We source our products from leading manufacturers in the industry, so you can be confident in the quality and performance of our offerings. From high-end graphics cards to reliable power supplies, we have everything you need to build or upgrade your computer.</p>
+							<ul class="actions">
+								<li><a href="about-us.html" class="button next">Read more</a></li>
+							</ul>
+						</div>
+					</section>
+					
+					<!-- Featured Products -->
+							<section class="tiles">
+								<article>
+									<span class="image">
+										<img src="images/product-1-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>Hard Disks</h3>
+
+										<p><del>$99.00 </del> <strong>$79.00</strong></p>
+
+										<p>A hard disk is a type of computer storage device that uses magnetic storage to store and retrieve digital data. The hard disk consists of one or more rotating disks, called platters, which are coated with a magnetic material. </p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/product-2-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>Memory Cards</h3>
+
+										<p><del>$199.00</del> <strong>$179.00</strong></p>
+
+										<p>Memory, also known as RAM (Random Access Memory),
+											 is a type of computer hardware that stores data 
+											 temporarily while a computer is running. It is an essential 
+											 component of a computer system that allows the computer to 
+											 access and process data quickly.</p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/product-3-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>CPU</h3>
+
+										<p><del>$99.00</del> <strong> $79.00</strong></p>
+
+										<p>Upgrade Your Performance with Our Premium CPUs - Shop Our Promotion Now and Get the Best Deals!</p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/product-4-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>Mouse</h3>
+
+										<p><del>$99.00</del> <strong> $79.00</strong></p>
+
+										<p>"Enhance Your Gaming and Productivity - Shop Our Mouse Promotion Now and Save on the Best Selection!"</p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/product-5-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>Keyboards</h3>
+
+										<p><del>$99.00</del> <strong> $79.00</strong></p>
+
+										<p>"Enhance Your Gaming and Productivity - Shop Our Mouse Promotion Now and Save on the Best Selection!"</p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+								<article>
+									<span class="image">
+										<img src="images/product-6-720x480.jpg" alt="" />
+									</span>
+									<header class="major">
+										<h3>HeadSets</h3>
+
+										<p><del>$99.00</del> <strong> $79.00</strong></p>
+
+										<p>"Get Fully Immersed in Your Audio Experience - Shop Our Headset Promotion Now and Save!"</p>
+
+										<div class="major-actions">
+											<a href="product-details.html" class="button small next">View Product</a>
+										</div>
+									</header>
+								</article>
+							</section>
+
+					<!-- Testimonials -->
+					<section>
+						<div class="inner">
+							<header class="major">
+								<h2>Testimonials</h2>
+							</header>
+							<div class="row">
+								<div class="col-6">
+									<p><em>"I have been a loyal customer of this IT hardware store for years and I can confidently say that they provide the best products and customer service in the industry. Their knowledgeable staff has always gone above and beyond to help me find the perfect hardware solutions for my business needs. Their wide selection of high-quality computer components and accessories has allowed me to build and upgrade my machines with ease, and their competitive prices keep me coming back time and time again. I highly recommend this IT hardware store to anyone looking for reliable products and exceptional customer service."</em></p>
+									<p><strong>- John Doe</strong></p>
+									
+								</div>
+
+								<div class="col-6">
+									<p><em>"I recently purchased computer components from this IT hardware store and I couldn't be more satisfied with my experience. The staff was knowledgeable and friendly, providing me with expert advice on the best components for my needs. Their selection of products was extensive, and the prices were very reasonable."</em></p>
+									<p><strong>- Jack Smith</strong></p>
+								</div>
+							</div>
+							<ul class="actions">
+								<li><a href="testimonials.html" class="button next">Read more</a></li>
+							</ul>
+						</div>
+					</section>
+
+					<!-- Blog Posts -->
+					<section class="tiles">
+						<article>
+							<span class="image">
+								<img src="images/blog-1-720x480.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3>Experience the Best in IT Hardware</h3>
+
+								<p><br> <span>John Doe</span> | <span>12/06/2020 10:30 </span> | <span>114</span></p>
+
+								<div class="major-actions">
+									<a href="blog-details.html" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+						</article>
+						<article>
+							<span class="image">
+								<img src="images/blog-2-720x480.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3>A Comprehensive Review of Megatek</h3>
+
+								<p><br> <span>John Doe</span> | <span>12/06/2020 10:30 </span> | <span>114</span></p>
+
+								<div class="major-actions">
+									<a href="blog-details.html" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+						</article>
+						<article>
+							<span class="image">
+								<img src="images/blog-3-720x480.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3>Find Out Why Megatek is the Top Destination for Computer Enthusiasts Everywhere</h3>
+
+								<p><br> <span>John Doe</span> | <span>12/06/2020 10:30 </span> | <span>114</span></p>
+
+								<div class="major-actions">
+									<a href="blog-details.html" class="button small next scrolly">Read Blog</a>
+								</div>
+							</header>
+						</article>
+					</section>
+				</div>
+
+				<!-- Footer -->
+				<footer id="footer">
+					<div class="inner">
+						<ul class="icons">
+							<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>Copyright © 2023 Megatek </li>
+							
+						</ul>
+					</div>
+				</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
